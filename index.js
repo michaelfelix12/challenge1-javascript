94ec8a1c1710c0d584b7a80501051f504cc15816
Michael <m.felixatmadja@gmail.com>
2022-11-03 16:18:20 +0700

    [ADD]

---
+++ index.js
@@ -0,0 +1,17 @@
+const phi = Math.PI
+
+
+function luasBalok(){
+    luasBalok = phi * r * r
+    console.log(luasBalok)
+}
+
+function luasLingkaran(){
+    luasBalok = phi * r * r
+    console.log(luasBalok)
+}
+
+function luasBalok(){
+    luasBalok = phi * r * r
+    console.log(luasBalok)
+}
